2f7d8a22334046c79c315dbfa9244cbb0b7e9a07
narayanan1994 <nanu.s94@gmail.com>
2023-09-10 08:34:35 +0530

    handlebars and html code cleanup, general cleanup

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,6 @@ router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    activeAddProduct: true,
     formCss: true,
     productCss: true
   });
--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,7 @@ const adminData = require("./admin");
 
 const router = express.Router();
 
+// GET - /
 router.get("/", (req, res, next) => {
   const products = adminData.products;
   // this automatically picks shop.pug/shop.hbs/shop.ejs file from views folder, we dont have to specify file extension
@@ -11,10 +12,7 @@ router.get("/", (req, res, next) => {
     prods: products,
     pageTitle: "Shop",
     path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCss: true,
-    layout: false // for handlebar, the render() takes this value as layout:true by default, we can overwrite it with setting false
+    productCss: true
   });
 });
 
